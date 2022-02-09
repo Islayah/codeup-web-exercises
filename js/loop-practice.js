a3224764c24d5c075ac0b9e477188ce5e2c60db1
Keith Benjamin <keith.i.benj@gmail.com>
2022-02-09 14:34:50 -0600

    Added loop practice.

---
+++ js/loop-practice.js
@@ -0,0 +1,17 @@
+// While Loops Notes
+/*let incrementor = 1;
+while (incrementor < 5) {
+    console.log(incrementor++)
+}
+
+let incrementor = 0;
+while (incrementor > 0) {
+    console.log(incrementor--);
+}
+console.log("The loop has ended.");*/
+
+// TODO: Use a while loop to output the numbers 8-50
+let x = 8;
+while (x <= 50) {
+    console.log(x++)
+}
