7026460571844b036cde5df0a985082e4ed4282d
Keith Benjamin <keith.i.benj@gmail.com>
2022-03-14 11:10:39 -0500

    Added mouse event exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -22,9 +22,23 @@
 /*$('.codeup').css('border', '1px solid red');*/
 
 // Element Selectors
+/*
 $('li').css('font-size', '20px');
 $('h1, p, li').css('color', 'red');
 $(document).ready(function () {
     var contents = $('title').html();
     alert(contents);
-})
+})*/
+
+// Moust Event Exercise
+$('#title').click(function () {
+    $(this).css('background-color', '#F00');
+});
+
+$('p').dblclick(function () {
+    $(this).css('font-size', '18px');
+});
+
+$('li').hover(function () {
+    $(this).css('color', '#F00');
+});
