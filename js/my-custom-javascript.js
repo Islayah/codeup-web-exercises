4e957dce968c372b90f73fb88627d2e8cc03d27d
Keith Benjamin <keith.i.benj@gmail.com>
2022-03-14 10:51:19 -0500

    Added completed selectors exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,22 +1,30 @@
 "use strict";
 
 
-/*
-window.onload = function() {
+/*window.onload = function() {
     alert("The page has finished loading!");
-}
-*/
+}*/
 
-/*
-$(document).ready(function () {
+/*$(document).ready(function () {
     alert( 'The DOM has finished loading!' );
-});
-*/
+});*/
 
-$(document).ready(function () {
+// ID Selectors
+// Remember to use the selectors.. spent 10 min wondering why ('title-2') didnt work when it should have been ('#title-2')
+/*$(document).ready(function () {
+    var contents = $('#title').html();
+    alert(contents);
     var contents = $('#title-2').html();
     alert(contents);
-});
+});*/
 
-/*var contents = $('#title').html();
-    alert(contents);*/
+// Class Selectors
+/*$('.codeup').css('border', '1px solid red');*/
+
+// Element Selectors
+$('li').css('font-size', '20px');
+$('h1, p, li').css('color', 'red');
+$(document).ready(function () {
+    var contents = $('title').html();
+    alert(contents);
+})
